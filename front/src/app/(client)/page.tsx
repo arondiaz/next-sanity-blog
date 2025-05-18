6e7d32d21dac66a4f886e17377789b7b23dee125
Arón Díaz <arondiaz.cpn@gmail.com>
2025-05-18 17:18:48 -0300

    created first fetch to get posts

--- front/src/app/(client)/page.tsx
+++ front/src/app/(client)/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { client } from "../../../sanity/lib/client";
 
-export default function Home ()  {
-  return (
-    <div>page</div>
-  )
+async function getPosts() {
+  const query = `*[_type == "post"]{title, slug, publishedAt, excerpt, body, tags}`;
+
+  const data = client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log(posts);
+
+  return <div>page</div>;
 }
